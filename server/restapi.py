5bddd77a319c3d9ff9813961f28e1fc79e9fe4e3
VisaJE <eemil@live.com>
2018-09-23 16:11:53 +0300

    Works!

--- server/restapi.py
+++ server/restapi.py
@@ -5,32 +5,33 @@ import configparser
 
 app = Flask(__name__)
 cp = configparser.ConfigParser()
-cp.read('')
+cp.read('/opt/secrets/bailu_config')
 
-pgsql_conn = psycopg2.connect('dbname={} user={} host={} password={}'.format((cp['PGSQL_DATABASE'], cp['PGSQL_USER, PGSQL_HOST'], cp['PGSQL_PASSWORD']))
+pgsql_conn = psycopg2.connect('dbname={} user={} host={} password={}'.format(cp['DEFAULT']['PGSQL_DATABASE'], cp['DEFAULT']['PGSQL_USER'], cp['DEFAULT']['PGSQL_HOST'], cp['DEFAULT']['PGSQL_PASSWORD']))
 cursor = pgsql_conn.cursor()
 
 @app.route('/bailu/threshold', methods=['get'])
 def threshold():
-    return ('%s'%cp['TEMPERATURE_THRESHOLD'], 200)
+    return ('%s'%cp['DEFAULT']['TEMPERATURE_THRESHOLD'], 200)
 
 @app.route('/bailu/name/<int:name_id>', methods=['get'])
 def getName(name_id):
     query = 'SELECT name FROM %s WHERE serial=%s;'
-    params = (cp['PGSQL_TABLE'], int(name_id))
+    params = (cp['DEFAULT']['PGSQL_TABLE'], int(name_id))
     print(query, params)  
     cursor.execute(query, params)
     result = cursor.fetchone()
     if result is not None:
       return (result[0], 200)
-    else return ('Anonymous', 202)
+    else:
+      return ('Anonymous', 202)
 
 @app.route('/bailu/name/insert', methods=['post'])
 def insertName():
     name = request.args.get('name')
     serial = request.args.get('serial')
     query = 'UPDATE %s SET name=%s WHERE serial=%s;'
-    params = ( cp['PSQL_TABLE'], name, serial)
+    params = ( cp['DEFAULT']['PSQL_TABLE'], name, serial)
     print(query, params)  
     cursor.execute(query, params)
     if cursor.rowcount == 0:
