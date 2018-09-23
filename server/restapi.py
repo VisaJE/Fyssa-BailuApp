0667199497dbc23ad69090ebfe7d5530b55883f8
VisaJE <eemil@live.com>
2018-09-23 15:18:51 +0300

    Configuring from a file

--- server/restapi.py
+++ server/restapi.py
@@ -1,21 +1,23 @@
 from flask import Flask, request
-from config import *
 from time import gmtime, strftime
 import psycopg2
+import configparser
 
 app = Flask(__name__)
+cp = configparser.ConfigParser()
+cp.read('')
 
-pgsql_conn = psycopg2.connect('dbname={} user={} host={} password={}'.format(PGSQL_DATABASE, PGSQL_USER, PGSQL_HOST, PGSQL_PASSWORD))
+pgsql_conn = psycopg2.connect('dbname={} user={} host={} password={}'.format((cp['PGSQL_DATABASE'], cp['PGSQL_USER, PGSQL_HOST'], cp['PGSQL_PASSWORD']))
 cursor = pgsql_conn.cursor()
 
 @app.route('/bailu/threshold', methods=['get'])
 def threshold():
-    return ('%d'%TEMPERATURE_THRESHOLD, 200)
+    return ('%s'%cp['TEMPERATURE_THRESHOLD'], 200)
 
 @app.route('/bailu/name/<int:name_id>', methods=['get'])
 def getName(name_id):
     query = 'SELECT name FROM %s WHERE serial=%s;'
-    params = (PGSQL_TABLE, int(name_id))
+    params = (cp['PGSQL_TABLE'], int(name_id))
     print(query, params)  
     cursor.execute(query, params)
     result = cursor.fetchone()
@@ -28,7 +30,7 @@ def insertName():
     name = request.args.get('name')
     serial = request.args.get('serial')
     query = 'UPDATE %s SET name=%s WHERE serial=%s;'
-    params = ( PSQL_TABLE, name, serial)
+    params = ( cp['PSQL_TABLE'], name, serial)
     print(query, params)  
     cursor.execute(query, params)
     if cursor.rowcount == 0:
