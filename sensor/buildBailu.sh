d0e16540d30139aee580dd2367ba677267e9a594
VisaJE <eemil@live.com>
2018-10-27 18:28:20 +0300

    Removed Core libs from git. By fefault should be placed in the folder containing Fyssa-BailuApp

--- sensor/buildBailu.sh
+++ sensor/buildBailu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir buildfolder
 cd buildfolder
-cmake -G Ninja -DMOVESENSE_CORE_LIBRARY=../MovesenseCoreLib/ -DCMAKE_TOOLCHAIN_FILE=../MovesenseCoreLib/toolchain/gcc-nrf52.cmake ../bailu_app
+cmake -G Ninja -DMOVESENSE_CORE_LIBRARY=../../MovesenseCoreLib/ -DCMAKE_TOOLCHAIN_FILE=../../MovesenseCoreLib/toolchain/gcc-nrf52.cmake ../bailu_app
 
 ninja dfupkg
 cp movesense_dfu.zip ..
--- sensor/buildBailu_standalone.sh
+++ sensor/buildBailu_standalone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir buildSfolder
 cd buildSfolder
-cmake -G Ninja -DMOVESENSE_CORE_LIBRARY=../MovesenseCoreLib/ -DCMAKE_TOOLCHAIN_FILE=../MovesenseCoreLib/toolchain/gcc-nrf52.cmake ../bailu_app_standalone
+cmake -G Ninja -DMOVESENSE_CORE_LIBRARY=../../MovesenseCoreLib/ -DCMAKE_TOOLCHAIN_FILE=../../MovesenseCoreLib/toolchain/gcc-nrf52.cmake ../bailu_app_standalone
 
 ninja dfupkg
 cp movesense_dfu.zip ../movesense_dfu_s.zip
